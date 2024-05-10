19d84b3a6fc17424fb2b53c728abec32a02232ba
ArthurYaxiongWang <arthurwyx@gmail.com>
2024-05-10 06:07:09 -0500

    The last version before demo on May 10

--- ChallengeZero.rb
+++ ChallengeZero.rb
@@ -15,19 +15,10 @@ puts "
 Subtotal: $#{sub_total}
 PST: $#{(PST).round(2)} - #{(PST_RATE * 100).round(2)}%
 GST: $#{GST} - #{GST_RATE * 100}%
-Grand Total: $#{grand_total}
+Grand Total: $#{grand_total} \n
 "
 
-# Question 2:
-if (grand_total <= 5.00)
-  puts "Pokect Change"
-elsif (grand_total > 5 && grand_total < 20)
-  puts "Wallet Time"
-else 
-  puts "Charge It!"
-end
-
-# Question 3:
+# Question 2 & 3:
 print "Enter the subtotal: "
 sub_total = gets.to_f
 
@@ -37,7 +28,7 @@ PST_RATE_Q3 = 0.07
 GST_Q3 = sub_total * GST_RATE
 PST_Q3 = sub_total * PST_RATE
 
-grand_total = sub_total + PST + GST
+grand_total = sub_total + GST_Q3 + PST_Q3
 
 puts "
 Subtotal: $#{sub_total}
@@ -45,3 +36,10 @@ PST: $#{(PST_Q3).round(2)} - #{(PST_RATE_Q3 * 100).round(2)}%
 GST: $#{(GST_Q3).round(2)} - #{(GST_RATE_Q3 * 100).round(2)}%
 Grand Total: $#{grand_total}
 "
+if (grand_total <= 5.00)
+  puts "Pokect Change"
+elsif (grand_total > 5 && grand_total < 20)
+  puts "Wallet Time"
+else 
+  puts "Charge It!"
+end
